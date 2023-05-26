728f6b8f313cbd217f0686ec3fa545fad167e762
Ranjit Pote <116176451+Ranjitpote@users.noreply.github.com>
2023-05-26 19:07:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
 //your code here!
+document.addEventListener("DOMContentLoaded", function() {
+  const list = document.getElementById("infi-list");
+
+  for (let i = 1; i <= 10; i++) {
+    const li = document.createElement("li");
+    li.textContent = "List Item " + i;
+    list.appendChild(li);
+  }
+
+  function addMoreItems() {
+    for (let i = 1; i <= 2; i++) {
+      const li = document.createElement("li");
+      li.textContent = "List Item " + (list.childElementCount + i);
+      list.appendChild(li);
+    }
+  }
+	
+  list.addEventListener("scroll", function() {
+    const scrollableHeight = list.scrollHeight - list.clientHeight;
+    if (list.scrollTop === scrollableHeight) {
+      addMoreItems();
+    }
+  });
+});
 
